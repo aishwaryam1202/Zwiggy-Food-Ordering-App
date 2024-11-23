167288c8ff1e50f8bdba4a9377439d8ab28f2527
Aishwarya Manoharan <74893699+aishwaryam1202@users.noreply.github.com>
2024-11-23 12:22:09 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,16 @@
+const heading = React.createElement(
+  "div",
+  {},
+  [React.createElement("div", {key: 'div1'}, [
+    React.createElement("h1", {key: "1"}, "Hello World "),
+    React.createElement("h2", {key: "2"}, "Hello World "),
+  ]),
+  React.createElement("div", {key: 'div2'}, [
+    React.createElement("h1", {key: "1"}, "Hello World "),
+    React.createElement("h2", {key: "2"}, "Hello World "),
+  ])]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading); // will be an object
+root.render(heading); // render convert it into HTML element and gives to div
