5ed6dc15b166036abee9b0f543fe5a6d63ae6459
Aishwarya Manoharan <74893699+aishwaryam1202@users.noreply.github.com>
2024-11-23 19:33:36 +0530

    Added Script to run

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "div",
-  {},
-  [React.createElement("div", {key: 'div1'}, [
-    React.createElement("h1", {key: "1"}, "Hello World "),
-    React.createElement("h2", {key: "2"}, "Hello World "),
-  ]),
-  React.createElement("div", {key: 'div2'}, [
-    React.createElement("h1", {key: "1"}, "Hello World "),
-    React.createElement("h2", {key: "2"}, "Hello World "),
-  ])]
-);
+const InnerComponent = () => {
+  return <div> Hello Aishu</div>;
+};
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      {InnerComponent()}
+      <InnerComponent />
+      <div> Hello World</div>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(heading); // will be an object
-root.render(heading); // render convert it into HTML element and gives to div
+root.render(<HeadingComponent />);
